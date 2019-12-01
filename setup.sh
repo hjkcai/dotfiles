739a45ddb3ad5679095c321ca52a49e33e351793
feihua <feihua@tencent.com>
2019-12-01 11:02:35 +0800

    feat(setup): add setup of agkozak

--- setup.sh
+++ setup.sh
@@ -4,6 +4,11 @@
 # TODO
 curl https://raw.githubusercontent.com/hjkcai/dotfiles/master/zshrc > $HOME/.zshrc
 
+# agkozak zsh theme
+[[ ! -d $ZSH_CUSTOM/themes ]] && mkdir $ZSH_CUSTOM/themes
+git clone https://github.com/agkozak/agkozak-zsh-prompt $ZSH_CUSTOM/themes/agkozak
+ln -s $ZSH_CUSTOM/themes/agkozak/agkozak-zsh-prompt.plugin.zsh $ZSH_CUSTOM/themes/agkozak.zsh-theme
+
 # Node.js
 echo Installing tj/n
 export N_NODE_MIRROR=https://npm.taobao.org/mirrors/node
