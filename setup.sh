64eef76ffff4fc08a93989b86afe425a7a11d7bc
Jingkun Hua <huajingkun1214@gmail.com>
2020-07-02 12:39:29 +0800

    feat: auto install oh-my-zsh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,9 @@
 # Setup script for new machines
 
 # oh-my-zsh
-# TODO
+RUNZSH=no sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 curl https://raw.githubusercontent.com/hjkcai/dotfiles/master/zshrc > $HOME/.zshrc
 
 # agkozak zsh theme
